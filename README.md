0827305f0c2841198fcbaf9e526fea912c43cf63
josephdakotajohnson <josephdakotajohnson@gmail.com>
2023-09-21 12:22:43 -0500

    Finished pulling user input from local storage and the ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # work-planner
 I want to create a way to organize my work schedule online.
+
+## Description
+
+ - I wanted to create a way to for people to organize their work schedule online.
+
+## Table of Contents
+
+  - [Credits](#credits)
+  - [Badges](#badges)
+  - [Features](#features)
+  - [Questions](#questions)
+
+## Credits
+
+ - This website was coded by Joseph Dakota Johnson with starter code provided.
+
+ - Instructor Caleb Crum helped me during class by showing me ways to add the time-block classes of past, present, and future. [Caleb's GitHub account](https://github.com/CalebCrumInstructor)
+
+ - Tutor Alexis Gonzalez helped me during a tutoring session by helping me figure out the correct perspective to think on when researching and implementing coding and by helping me figure out how to pull from local storage with a kei.
+
+ - My brother (boot camp graduate) helped out by helping me get started on creating my variables for saving to local storage. [Dylan's GitHub account](https://github.com/dylanstormjohnson)
+
+## Badges
+
+ - ![HTML](https://img.shields.io/badge/-HTML5-black?style=flat-square&logo=html5)
+ - ![CSS](https://img.shields.io/badge/-CSS3-black?style=flat-square&logo=css3)
+ - ![JavaScript](https://img.shields.io/badge/logo-javascript-blue?logo=javascript)
+
+## Features
+
+ - There is a title, a description, and a display of the current time.
+ - When you type in something into the middle div of each row and click the save button in that row, your 'event' gets saved to local storage.
+ - When you load the page again, your events saved in local storage appear where you saved them.
+ - All the center divs of each row display as gray if the time block is past the current hour, red if the time block is within the current hour, and green if the time block is before the current hour.
+ - You may schedule events in the current day from the hours of 9am to 5pm.
+
+## Questions
+
+ - If you have any questions for me, my GitHub account is [Dakota's GitHub account](https://github.com/josephdakotajohnson)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,57 +2,47 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+
+  // Save user input to local storage by time-block.
   function saveToLocalStorage() {
-    var event = $(this).siblings(".description").attr('id');
-    var time = $(this).parent().text();
-    console.log(event);
-    localStorage.setItem(time, event);
+    var userInput = $(this).siblings(".description").val();
+    var parentId = $(this).parent().attr('id');
+    console.log(userInput);
+    console.log(parentId);
+    localStorage.setItem(parentId, userInput);
   }
 
     $(".saveBtn").on("click", saveToLocalStorage);
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  var militaryTime9To5;
-
+  // Apply the correct class and get user input from local storage.
   function timeIntervals() {
     var timePeriods = $(".time-block"); 
     var currentTime = dayjs().hour();
 
     timePeriods.each(function() {
-        if (parseInt($(this).attr('id')) === currentTime) {
+      var id = $(this).attr('id');
+        if (parseInt(id) === currentTime) {
             $(this).addClass("present");
-        } else if (parseInt($(this).attr('id')) > currentTime) {
+        } else if (parseInt(id) > currentTime) {
             $(this).addClass("future");
         } else {
             $(this).addClass("past");
         }
+    var valueSaved = localStorage.getItem(id);
+    console.log(valueSaved);
+    var textAreaChild = $(this).children().eq(1).val(valueSaved);
+    console.log(textAreaChild);
     });
 
   }
 
   timeIntervals();
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Current Time in the Header
   function realTimeInterval() {
   var currentDate = new Date();
   currentDate = $('#currentDay').text(currentDate);
   }
-  
+
   setInterval(realTimeInterval, 1000);
 });
